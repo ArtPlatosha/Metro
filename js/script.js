0c1c4798f46583a5ee4c1e061eeff87b4f462599
ArtPlatosha <zipex98@gmail.com>
2023-05-10 13:10:14 +0300

    commit: 9th commit
    ++mobile navigation (JS)

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 // activate the hamburger menu and mobile navigation
 const hamb = document.querySelector('.hamb');
+const nav = document.querySelector('.mobile-nav');
 
 hamb.addEventListener('click', function() {
 	this.classList.toggle('active');
+	nav.classList.toggle('slide');
 });
