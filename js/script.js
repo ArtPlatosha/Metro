b8baf85a066fb823160b0fad3b911a0b4152183f
ArtPlatosha <zipex98@gmail.com>
2023-05-11 10:42:19 +0300

    commit: 23rd commit
    add sticky navbar (JS)

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
+// add sticky navbar
+const header = document.querySelector('header');
+
+window.addEventListener('scroll', function() {
+	header.classList.toggle('sticky', window.scrollY > 0);
+});
+
 // activate the hamburger menu and mobile navigation
 const hamb = document.querySelector('.hamb');
 const nav = document.querySelector('.mobile-nav');
